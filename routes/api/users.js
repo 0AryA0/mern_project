4630afac33f1416937408bfc9f315e3e1db89bec
arya <aryaebrahimi3000@gmail.com>
2020-07-27 20:34:47 +0430

    mern project

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 
+//load user model
+const User = require('../../models/User');
 
 /*
     @route GET api/users/test
@@ -8,6 +12,76 @@ const router = express.Router();
     @access Public
 */
 
-router.get('/test', (req, res) => res.json({message: "users works"}));
+router.get('/test', (req, res) => res.json({ message: "users works" }));
+
+/*
+    @route GET api/users/register
+    @desc register user
+    @access Public
+*/
+
+router.post('/register', (req, res) => {
+
+    const email = req.body.email;
+    console.log(email);
+    //check if email is available
+    User.findOne({ email: req.body.email }, (err, result) => {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            if (result != null) {
+                console.log('email address is ' + email);
+                return res.status(400).json({ email: 'email already exists' });
+            }
+            else {
+                console.log("adding a new user");
+                const avatar = gravatar.url(req.body.email, {
+                    s: '200', //size
+                    r: 'pg', //rating
+                    d: 'mm' //default
+                });
+
+                //create user
+
+                let user = new User({
+                    name: req.body.name,
+                    email: req.body.email,
+                    avatar: avatar,
+                    password: req.body.password
+                });
+
+                bcrypt.genSalt(10, (err, salt) => {
+                    bcrypt.hash(user.password, salt, (err, hash) => {
+                        if(err) throw err;
+            
+                        user.password = hash;
+                        user.save((err, result) => {
+                            if(err) {
+                                console.log(err);
+                            }
+                            else {
+                                console.log("user added successfully");
+                                return res.status(200).json({ status: 'user registered successfully' });
+                            }
+                        });
+                    });
+                });
+            }
+        }
+    });
+
+
+
+    //generate a salt with bcrypt and hash the password
+
+
+    
+    
+    
+
+
+
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyparser = require('body-parser');
+
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
-
 const app = express();
 
+/*
+app.use(bodyparser.urlencoded({extended: true}));
+app.use(bodyparser.json);
+
+*/
 
 //db config (database uri from keys)
 const db = require('./config/keys').mongoURI;
+
+app.use(bodyparser.json({type: '*/*'}));
+app.use(bodyparser.urlencoded({ extended: true}));
 
 //connect to mongoDB
 mongoose.connect(db, {useNewUrlParser: true, useUnifiedTopology: true})
@@ -16,14 +25,19 @@ mongoose.connect(db, {useNewUrlParser: true, useUnifiedTopology: true})
 .catch(err => console.log(err));
 
 
-app.get('/', (req, res) => res.send("Hello World"));
-
-
 //user routes
+//app.use(express.json());
+
+
 app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
 
 
-const PORT = process.env.PORT || 3000;
+
+
+const PORT = process.env.PORT || 7000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+app.get('/', (req, res) => res.send("Hello World"));
